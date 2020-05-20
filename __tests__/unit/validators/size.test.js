ad959d4a863ccc58295e302de56e118801544440
Sambhav Kothari <skothari44@bloomberg.net>
2020-05-20 14:21:20 +0100

    Allow specifying files to match (#5)
    * fix the parameter list in aftervalidate
    
    * fix tests
    
    * Update dependencies
    
    Updates some of the dependencies to fix vulnerabilities reported by
    Snyk:
    
    - https://snyk.io/vuln/SNYK-JS-CODECOV-543183
    - https://snyk.io/vuln/SNYK-JS-CODECOV-548879
    - https://snyk.io/vuln/SNYK-JS-DOTPROP-543489
    - https://snyk.io/vuln/SNYK-JS-YARGSPARSER-560381
    - https://snyk.io/vuln/npm:braces:20180219
    - https://snyk.io/vuln/npm:eslint:20180222
    
    The new version of Standard has some stricter linting which required
    some minor style changes in some files.
    
    * Allow specifying files to match

--- __tests__/unit/validators/size.test.js
+++ __tests__/unit/validators/size.test.js
@@ -274,6 +274,17 @@ describe('PR size validator', () => {
     expect(validation.validations[1].status).toBe('pass')
     expect(validation.validations[2].description).toBe('PR size for total additions + deletions is OK!')
     expect(validation.validations[2].status).toBe('pass')
+
+    settings.match = ['not_*.js']
+
+    validation = await size.validate(createMockContext(files), settings)
+    expect(validation.status).toBe('pass')
+    expect(validation.validations[0].description).toBe('PR size for additions is OK!')
+    expect(validation.validations[0].status).toBe('pass')
+    expect(validation.validations[1].description).toBe('PR size for deletions is OK!')
+    expect(validation.validations[1].status).toBe('pass')
+    expect(validation.validations[2].description).toBe('PR size for total additions + deletions is OK!')
+    expect(validation.validations[2].status).toBe('pass')
   })
 
   test('handles empty ignore args', async () => {
